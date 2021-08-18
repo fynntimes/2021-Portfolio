96434bf15b5779dab9b8c6ab06723520b44a06f8
Faizaan Datoo <fdatoo7@gmail.com>
2021-08-18 18:50:15 -0400

    Add new photo and resume

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -12,7 +12,7 @@ export default function AboutMe(props) {
                 </div>
                 <div id="aboutButtons">
                     <LinkBtn href="https://github.com/faizaand/" target="_blank"><i class="fab fa-github"></i>GitHub</LinkBtn>
-                    <LinkBtn href="downloads/resume-dec-2020.pdf" target="_blank"><i class="fas fa-paperclip"></i>Resume</LinkBtn>
+                    <LinkBtn href="downloads/resume-aug-2021.pdf" target="_blank"><i class="fas fa-paperclip"></i>Resume</LinkBtn>
                     <LinkBtn href="mailto:fad35@cornell.edu"><i class="fas fa-inbox"></i>Email</LinkBtn>
                     <LinkBtn href="https://www.linkedin.com/in/faizaand/" target="_blank"><i class="fab fa-linkedin"></i>LinkedIn</LinkBtn>
                 </div>
